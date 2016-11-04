b4d20b893441e54d9f28a242386014a81ef2fd00
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-11-04 14:34:27 +0000

    Refactor to remove extra comments

--- spec/yatzy_test.rb
+++ spec/yatzy_test.rb
@@ -1,19 +1,10 @@
 require 'minitest/autorun'
-# require './blog'
 
 def score(category, dice)
   dice.reduce(&:+)
 end
 
 class TestYatzy < Minitest::Test
-
-  # def setup
-  #   @blog = Blog.new
-  # end
-
-  # def test_title_is_treehouse
-  #   assert_equal "Treehouse Blog", @blog.title
-  # end
   def test_chance_all_ones
     assert_equal score(:chance, [1,1,1,1,1]), 5
   end
