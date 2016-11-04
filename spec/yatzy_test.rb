4cb8b294a14441eaa197f4259222c7380504a8f3
Chris Mytton <chrismytton@gmail.com>
2016-11-04 15:39:16 +0100

    Add failing test for :yatzy category

--- spec/yatzy_test.rb
+++ spec/yatzy_test.rb
@@ -12,4 +12,8 @@ class TestYatzy < Minitest::Test
   def test_chance_all_twos
     assert_equal score(:chance, [2,2,2,2,2]), 10
   end
+
+  def test_yatzy_all_ones
+    assert_equal score(:yatzy, [1, 1, 1, 1, 1]), 50
+  end
 end
