15051419125f39a4b3596e25fa198df34787a103
Chris Mytton <chrismytton@gmail.com>
2016-11-04 15:52:38 +0100

    Re-order assert_equal arguments

--- spec/yatzy_test.rb
+++ spec/yatzy_test.rb
@@ -14,18 +14,18 @@ end
 
 class TestYatzy < Minitest::Test
   def test_chance_all_ones
-    assert_equal score(:chance, [1,1,1,1,1]), 5
+    assert_equal 5, score(:chance, [1,1,1,1,1])
   end
 
   def test_chance_all_twos
-    assert_equal score(:chance, [2,2,2,2,2]), 10
+    assert_equal 10, score(:chance, [2,2,2,2,2])
   end
 
   def test_yatzy_all_ones
-    assert_equal score(:yatzy, [1, 1, 1, 1, 1]), 50
+    assert_equal 50, score(:yatzy, [1, 1, 1, 1, 1])
   end
 
   def test_yatzy_all_different
-    assert_equal score(:yatzy, [1, 2, 3, 4, 5]), 0
+    assert_equal 0, score(:yatzy, [1, 2, 3, 4, 5])
   end
 end
