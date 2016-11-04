2a9c4ec554d64f2f764a42031757285ca394630c
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-04 14:29:51 +0000

    Add second chance score test

--- spec/yatzy_test.rb
+++ spec/yatzy_test.rb
@@ -14,7 +14,11 @@ class TestYatzy < Minitest::Test
   # def test_title_is_treehouse
   #   assert_equal "Treehouse Blog", @blog.title
   # end
-  def test_chance
+  def test_chance_all_ones
     assert_equal score(:chance, [1,1,1,1,1]), 5
+  end
+
+  def test_chance_all_twos
+    assert_equal score(:chance, [2,2,2,2,2]), 10
   end
 end
